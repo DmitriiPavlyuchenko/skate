032a619589af577005eae783e41dfb4b6c31a011
DmitriiPavlyuchenko <dorshichek1996@gmail.com>
2022-07-05 23:26:15 +0300

    Add pages routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,31 @@ const routes: Array<RouteRecordRaw> = [
     name: "home",
     component: () => import("@/views/HomePage.vue"),
   },
+  {
+    path: "/news",
+    name: "news",
+    component: () => import("/src/views/NewsPage.vue"),
+  },
+  {
+    path: "/series",
+    name: "series",
+    component: () => import("@/views/SeriesPage.vue"),
+  },
+  {
+    path: "/skaters",
+    name: "skaters",
+    component: () => import("@/views/SkatersPage.vue"),
+  },
+  {
+    path: "/brands",
+    name: "brands",
+    component: () => import("@/views/BrandsPage.vue"),
+  },
+  {
+    path: "/videos",
+    name: "videos",
+    component: () => import("@/views/VideosPage.vue"),
+  },
 ];
 
 const router = createRouter({
